fc92aaedec4eb8789cc684605d5bcdc293aab38b
alenash1988 <Aleonash1988@mail.ru>
2024-08-15 18:28:04 +0900

    Add files via upload

--- modul_1_5.py
+++ modul_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = 3, 4, "milk", True
 print(immutable_var)
-#immutable_var[0] = 5
+#immutable_var[0] = 5 (потому что кортеж это не изменняемый обьект, если мы нажмем Run, программа даст ошибку)
 mutable_list = ["agg", "milk", "bred"]
 mutable_list[0] = "meat"
 print(mutable_list)
